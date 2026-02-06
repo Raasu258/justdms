c069e9fa3fa04be884a4b8a2577fd7aed6f38fdf
Lars <129963861+Raasu258@users.noreply.github.com>
2026-02-06 23:36:14 +0100

    First init push. Added raw BlobStore methods

---
+++ BlobStore/BlobStore.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace JustDMS.BlobStore;
+
+using System.IO;
+using System.Security.Cryptography;
+
+internal class BlobStore
+{
+    private readonly string _rootDir;
+
+    public BlobStore(string rootDirectory)
+    {
+        _rootDir = rootDirectory ?? throw new ArgumentNullException(nameof(rootDirectory));
+        Directory.CreateDirectory(_rootDir);
+    }
+    
+
+    internal string Put(Stream content)
+    {
+        
+    }
+
+    internal Stream Get(string hash)
+    {
+        
+    }
+
+    internal bool Exists(string hash)
+    {
+        
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using JustDMS.BlobStore;
+
+internal class Main
+{
+
+    
+    public static void main(string[] args)
+    {
+        string workingDir = Directory.GetCurrentDirectory();
+        
+        var DataStore = new BlobStore(workingDir);
+        
+    }
+}
